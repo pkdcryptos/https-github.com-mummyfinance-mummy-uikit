1b739d6132d650d8fb15f3e61938e9bfbc190e6c
mummyfinance <financemummy@gmail.com>
2021-02-26 12:53:19 -0300

    Spinner logo

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -15,7 +15,7 @@ const rotate = keyframes`
 const SpinnerIcon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 96 96" {...props}>
-      <image width="96" height="96" href="images/scarab/9.png"/>
+      <image width="96" height="96" href="images/scarab/spinner.png"/>
     </Svg>
   );
 };
